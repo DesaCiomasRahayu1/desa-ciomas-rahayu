b00f181e0ba9ccc3f7bee35147175c373497a55c
DesaCiomasRahayu1 <144506293+DesaCiomasRahayu1@users.noreply.github.com>
2023-10-30 12:43:26 +0700

    update sejarah

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,6 +14,20 @@ import Sidebar from "../components/Sidebar.vue";
       <img src="@/assets/strukturdesaciomasrahayu.png" alt="" />
     </div>
   </main>
+  <main class="flex">
+    <Sidebar />
+
+    <div class="flex-1 p-10">
+      <p class="text-lg font-semibold">Sejarah</p>
+      <p>
+        “Pada zaman Belanda Desa Ciomas Rahayu masih bersatu dengan Desa Ciomas, Desa Pagelaran, Panggugah. Memasuki tahun 1980 mulai pemekaran desa dimulai dengan Padasuka yang sekarang menjadi Kelurahan Padasuka. Tahun 1982 Desa Ciomas Rahayu mulai melakukan proses pemekaran dan baru tahun 1984 secara resmi menerima SK untuk melakukan pemekaran. Penempatan Kantor Desa sebelum pemekaran terletak di Kantor PDAM Citra Pakuan setelah dilakukan pemekaran dengan status kepemimpinan Pejabat Sementara Bapak Kyai Arga Prawira selama 2 Tahun kantor Desa Ciomas Rahayu Pindah ke rumah Bapak Kyai Arga Prawira. 
+Pada tahun 1986 dilakukan pemilihan dengan terdiri dari 4 calon, dan yang terpilih menjadi kepala Desa yaitu  H. Jaya Rukmantara lalu kantor desa juga ikut pindah ke rumah H. Jaya Rukmantara. Selama menjabat pada tahun 1987 ada program Bantuan Presiden dengan memberi anggaran sekitar Rp. 2.000.000 diperuntukan untuk membangun sebuah kantor desa. Pada tahun 1988 perpindahan kantor desa dari rumah Kades H. Jaya Rukmantara ke kantor desa yang sekarang. Pada tahun 1986 kepala desa masih menjabat sampai 8 tahun dalam satu periode. 
+Ditahun 1994 periode Kepala Desa habis dan dijabat oleh Pejabat Sementara Bapak Hidup sampai tahun 1996. Tahun 1995 pembebasan perumahan Villa Ciomas Indah dengan luas sekitar 27 Ha dari PT. Gn. Salak. Tahun 1996 perkembangan ekonomi dan Villa Ciomas Indah sudah berkembang Adapun Pemilihan Kepala Desa dengan 8 Rw dan yang terpilih Bapak Hidup. Ditahun 1998 pembagian menjadi 14 Rw. Pada tahun 2001 menambah 1 Rw menjadi 15 Rw. 
+Ditahun 2004 periode kepala desa habis dan diganti Pelaksana Tugas Bapak Agus Budiansyah dan memasuki tahun 2005 dilakukan Pemilihan Kepala Desa terdapat 5 kandidat dan Bapak Jasarudin terpilih sebagai Kepala Desa Periode 2005-2011. Secara perekonomian kepala desa Jasarudin melakukan pembentukan Perdagangan di gerbang Villa Ciomas Indah. Pada tahun 2006 melakukan pembebasan perumahan Andika dan memullai rehabilitasi Kantor Desa. 
+Ditahun 2011 dilakukan pemilihan kembali dan H. Jaya Rukmantara terpilih sebagai Kepala Desa Ciomas Rahayu kedua kalinya, jabatan diperiode keduanya dari tahun 2011 – 2017. Ditahun 2011 pun mulai pembebasan Ciomas River View & GreenHill, 2014 Pembebasasn Ciomas Village. Pada tahun 2015 sudah mulai ada dana dari Pemerintah Kabupaten Bogor yaitu Dana Desa. 
+Tahun 2017 dilakukan pemilu dan Bapak Jumadi terpilih sebagai kepala desa Ciomas Rahayu Periode 2017-2023, ditahun 2017 sudah ada tambahan anggaran dengan dari Bantuan Provinsi. Pada tahun 2021 Kepala Desa Ciomas Rahayu Bapak Jumadi meninggal dunia dan dilanjutkan dengan Pejabat Sementara Saeful Jack Peter hingga 2023. 
+Pada tahun 2023 dilakukan pemilihan Kepala Desa dengan 4 calon dan Selih Silih Wati Terpilih sebagai Kepala Desa Periode 2023-2029. Pada awal pemimpinannya melakukan penabahan Rw dengan usulan Masyarakat bahwa Ciomas River View menjadi Rw 016.
+.”
 </template>
 
 <style>
