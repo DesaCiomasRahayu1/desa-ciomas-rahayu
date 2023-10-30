0379b0418153beacf70430c4239884a14154eb5c
DesaCiomasRahayu1 <144506293+DesaCiomasRahayu1@users.noreply.github.com>
2023-10-30 14:58:55 +0700

    update vt

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -13,8 +13,8 @@
     </div>
     <div class="grid place-items-center ml-auto mr-20">
       <iframe
-        width="500"
-        height="280"
+        width="400"
+        height="240"
         src="https://www.youtube.com/embed/zzIhnvU3HPk?si=0DF1sJDQxicxoEIn"
         title="YouTube video player"
         frameborder="0"
